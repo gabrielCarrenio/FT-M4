2ef6dede77bcff0057060f6047b7a6ebefa45294
Gabriel Carreño <carrenocarlosgabriel@gmail.com>
2022-07-19 17:29:12 -0300

    ...

--- 03-sequelize/homework/db/models/Ability.js
+++ 03-sequelize/homework/db/models/Ability.js
@@ -1,7 +1,19 @@
-const { DataTypes } = require('sequelize');
+const { DataTypes } = require("sequelize");
 
-module.exports = sequelize => {
-  sequelize.define('Ability', {
-
-  })
-}
+module.exports = (sequelize) => {
+  sequelize.define("Ability", {
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: "compositeUnique",
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+    mana_cost: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      unique: "compositeUnique",
+    },
+  });
+};
